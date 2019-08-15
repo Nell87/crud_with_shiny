7f5433fa1aaddaef955232d01ad0becaca4d4a74
Sara <sara.marlop87@gmail.com>
2019-08-15 02:08:01 +0200

    Refresh button + small changes

--- scripts/CRUD_app_shiny.R
+++ scripts/CRUD_app_shiny.R
@@ -8,7 +8,7 @@ if(require("pacman")=="FALSE"){
 
 pacman::p_load(rstudioapi,dplyr, ggplot2, lubridate, randomForest, caret,
                rpart,rpart.plot,tidyr, shiny, shinydashboard, rvest, DT,
-               ggthemes,RMySQL,rhandsontable,data.table,shinyjs,DBI, keyring)
+               ggthemes,RMySQL,rhandsontable,data.table,shinyjs,DBI, shinyBS)
 
 # Setwd (set current wd where is the script, then we move back to the
 # general folder)
@@ -22,13 +22,13 @@ source("./scripts/credentials.R")
 mydb = dbConnect(MySQL(), user=user, password=password, 
                  dbname=dbname, host=host, port=port)
 
-rm(user, password, dbname, host, port)
-
 dbListTables(mydb)
 
 recipes = fetch(dbSendQuery(mydb, "select * from recipes"))
 sources = fetch(dbSendQuery(mydb, "select * from sources"))
 ingredients = fetch(dbSendQuery(mydb, "select * from ingredients"))
+
+
 
 # newline<- data.frame(id=4, name="patata", id_sources=2, url="pa", 
 #                                minutes=60, temperature="caliente")
@@ -67,51 +67,74 @@ ui <- dashboardPage(
                  textInput(inputId = "recipe_insert_name", label= "Insert name", value=""),
                  selectInput(inputId = "recipe_select_source", label= "Insert source",
                              choices = c(1,2)),
-                 textInput(inputId = "recipe_insert_url", label= "Insert url", value=""),
-                 numericInput(inputId = "recipe_insert_time", label="Insert time", value="")
+                 textInput(inputId = "recipe_insert_url", label= "Insert url", value="")
+                 
                ),
                column(6,
+                 numericInput(inputId = "recipe_insert_time", label="Insert time", value=""),
                  selectInput(inputId = "recipe_select_temp", label= "Insert source",
                              choices = c("fria", "caliente")),
                  selectizeInput(inputId = "recipe_select_ingred", 
                                 label= "Insert main ingredients",
                                 choices= c(ingredients$name),selected = NULL, 
-                                multiple = TRUE),
-                 actionButton("save","save")
+                                multiple = TRUE)
                )
                    
+             ),
+             
+             fluidRow(
+               column(6,
+                actionButton("save","save")
+               ),
+               column(6,
+                # Restart the shiny session
+                tags$a(href="javascript:history.go(0)", 
+                       popify(tags$i(class="fa fa-refresh fa-5x"),
+                              title = "Reload", 
+                              content = "Click here to restart the Shiny session",
+                              placement = "right"))      
+               )
              )
       )
     )
   )
 )
  server <- function(input, output, session) {
+ 
+   #### PREPARING DATA _____________________________________________####     
+    
+   # Connect to databse
+   mydb <- reactive({
+     mydb = dbConnect(MySQL(), user=user, password=password, 
+                      dbname=dbname, host=host, port=port)
+   })
    
+   # RecipeS
+   recipes<- reactive({
+      recipes = fetch(dbSendQuery(mydb(), "select * from recipes"))
+   })
+   
+   #### OUTPUTS ____________________________________________________________####
+   
+   # Showing the exploring section
    output$exploring = renderRHandsontable({
-      # transform the df into a rhandsontable object
-     rhandsontable(recipes)   
+     rhandsontable(recipes())   
    })
    
+   # Showing the adding section
    output$adding = renderRHandsontable({
-     rhandsontable(recipes) 
+     rhandsontable(recipes()) 
    })
    
-   #saving the changes with the save button
+   # Saving the changes with the save button
    observeEvent(input$save,{
 
-    # newline<- data.frame(
-    #                      name=input$recipe_insert_name,
-    #                      id_sources=input$recipe_select_source, 
-    #                      url=input$recipe_insert_url, 
-    #                      minutes= input$recipe_insert_time,
-    #                      temperature= input$recipe_insert_temp)
-     
-     newline<- data.frame(name=input$recipe_insert_name,
+    newline<- data.frame(name=input$recipe_insert_name,
                           id_sources=input$recipe_select_source,
                           url=input$recipe_insert_url,
                           minutes= input$recipe_insert_time,
                           temperature=input$recipe_select_temp)
-
+    mydb = mydb()
     DBI::dbWriteTable(mydb, name="recipes", value=newline, append=TRUE,
                        row.names = FALSE)
 
