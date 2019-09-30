ceee7da43bf411826291e09f195e69a5f909c084
Sara <sara.marlop87@gmail.com>
2019-09-30 12:56:24 +0200

    Adding DB

---
+++ data/nell_cooking_rel_ingredients_recipes.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: saramarlop.com    Database: nell_cooking
+-- ------------------------------------------------------
+-- Server version	5.5.52-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `rel_ingredients_recipes`
+--
+
+DROP TABLE IF EXISTS `rel_ingredients_recipes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rel_ingredients_recipes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_ingredients` int(11) NOT NULL,
+  `id_recipes` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ingredients_rel_ingredients_recipes_idx` (`id_ingredients`),
+  KEY `recipes_rel_ingredients_recipes_idx` (`id_recipes`),
+  CONSTRAINT `ingredients_rel_ingredients_recipes` FOREIGN KEY (`id_ingredients`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `recipes_rel_ingredients_recipes` FOREIGN KEY (`id_recipes`) REFERENCES `recipes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rel_ingredients_recipes`
+--
+
+LOCK TABLES `rel_ingredients_recipes` WRITE;
+/*!40000 ALTER TABLE `rel_ingredients_recipes` DISABLE KEYS */;
+INSERT INTO `rel_ingredients_recipes` VALUES (38,1,1),(39,2,2),(40,3,3),(41,4,3),(46,29,33);
+/*!40000 ALTER TABLE `rel_ingredients_recipes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-30 12:50:19
